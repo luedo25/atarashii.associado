d50ad19eea275def6137fe2a9cc290a2e320c36e
luedo25 <luedo25@gmail.com>
2026-07-01 15:14:49 -0300

    usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -2,9 +2,24 @@
 
 // Sua lista oficial e fixa de alunos e senhas
 const listaAlunos = [
-    { usuario: "adriano.miguel", senha: "123456", nome: "Adriano Miguel" },
-    { usuario: "edson.fernandez", senha: "123456", nome: "Edson Fernandez" },
-    { usuario: "luiz.guilherme", senha: "123456", nome: "Luiz Guilherme" }
+    { usuario: "adriano.miguel", 
+	senha: "123456", 
+	nome: "Adriano Miguel", 
+	historico: [
+            { faixa: "Branca/Amarela", corFaixa: "#ffeb3b", corTexto: "#000", ano: "Janeiro / 2022", kihon: 7.9, kata: 7.9, kumite: 8.1, perguntas: 0.5, total: 24.4, media: 8.1 },
+            { faixa: "Amarela/Vermelha", corFaixa: "#ff0000", corTexto: "#fff", ano: "Junho / 2022", kihon: 7.7, kata: 7.7, kumite: 8.5, perguntas: 0.3, total: 24.2, media: 8.1 },
+            { faixa: "Vermelha/Laranja", corFaixa: "#e65100", corTexto: "#fff", ano: "Dezembro / 2022", kihon: 7.2, kata: 7.3, kumite: 7.4, perguntas: 0.5, total: 22.4, media: 7.5 },
+            { faixa: "Laranja/Verde", corFaixa: "#4caf50", corTexto: "#fff", ano: "Junho / 2023", kihon: 7.6, kata: 7.4, kumite: 7.6, perguntas: 0.5, total: 23.1, media: 7.7 },
+            { faixa: "Verde/Roxa", corFaixa: "#673ab7", corTexto: "#fff", ano: "Junho / 2024", kihon: 7.8, kata: 7.8, kumite: 8.0, perguntas: 0.5, total: 24.1, media: 8.0 }
+        ]	
+	},
+    { usuario: "edson.fernandez", 
+	senha: "123456", 
+	nome: "Edson Fernandez" 
+	},
+    { usuario: "luiz.guilherme", 
+	senha: "123456", 
+	nome: "Luiz Guilherme" }
 ];
 
 /**
